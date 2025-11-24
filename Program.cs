8aa75393dab8a47da92cd2e65691e65220c50b72
Manzanilla2510 <manzanillamancilla05@gmail.com>
2025-11-23 22:48:22 -0400

    Modify database connection handling in Program.cs
    Updated database connection string retrieval and added console output for connection URL.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PVeterianaria.Core.Interfaces;
 using PVeterianaria.Infraestructura.Data;
 using PVeterianaria.Infraestructura.Repositorio;
 using PVeterinaria.Infraestructura.Repositorio;
 
+var url = Environment.GetEnvironmentVariable("DATABASE");
+Console.Write($"La coneccion es esta {url}");
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PVeterianariaContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("PVeterianariaContext") ?? throw new InvalidOperationException("Connection string 'PVeterianariaContext' not found.")));
-
+    options.UseNpgsql(url));
 
 // Add services to the container.
+
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
 builder.Services.AddCors(options =>
 {
@@ -28,6 +32,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IAnimalRepositorio, AnimalRepositorio>();
 builder.Services.AddScoped<IVeterinarioRepositorio, VeterinarioRepositorio>();
 builder.Services.AddScoped<IRevisionRepositorio, RevisionRepositorio>();
